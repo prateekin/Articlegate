1870d8ef9e37113f6620445822ffb3fc998ea93c
Ravi <84930462+Ravikant-0332@users.noreply.github.com>
2021-11-13 17:48:09 +0530

    Update urls.py

--- oop_project_env/oop_project/user/urls.py
+++ oop_project_env/oop_project/user/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from . import views
 
 app_name='user'
 
 urlpatterns = [
-    path(),
+    path('', views.index ,name='index'),
+    path('signin/', views.signIn ,name='signIn'),
+    path('signup/', views.signUp ,name='signUp'),
 ]
