126a2c60ee869bf5526827d4b8c86a719a5d6b93
Ravi <84930462+Ravikant-0332@users.noreply.github.com>
2021-11-13 17:41:40 +0530

    Update views.py

--- oop_project_env/oop_project/user/views.py
+++ oop_project_env/oop_project/user/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    # Your Code
+    return render(request, 'user/index.html') 
+
+def signIn(request):
+    # Your Code
+    return render(request, 'user/login.html') 
+
+def signUp(request):
+    # Your Code
+    return render(request, 'user/register.html') 
+
